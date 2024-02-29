20216dea1e624cd693c411526be085f2a5e02f6b
Kishan Gajnotar <kishangajnotar80@gmail.com>
2024-02-29 22:07:39 +0530

    gg

---
+++ src/components/EventCreationForm.js
@@ -0,0 +1,153 @@
+// EventCreationForm.js
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const EventCreationForm = () => {
+  const [eventData, setEventData] = useState({
+    name: '',
+    startTime: '',
+    endTime: '',
+    location: '',
+    description: '',
+    category: '',
+    bannerImage: ''
+  });
+
+  const isEventDataEmpty = Object.values(eventData).some(value => value === '');
+
+  console.log("event data...", eventData)
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEventData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch('http://127.0.0.1:5000/events/create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: eventData.name,
+                starttime: eventData.startTime,
+                endtime: eventData.endTime,
+                location: eventData.location,
+                description: eventData.description,
+                category: eventData.category,
+                bannerImage: eventData.bannerImage
+            })
+        });
+        if (!response.ok) {
+            throw new Error('Failed to create event: ' + response.status);
+        }
+
+        alert('Event created successfully!');
+        setEventData({
+          name: '',
+          startTime: '',
+          endTime: '',
+          location: '',
+          description: '',
+          category: '',
+          bannerImage: ''
+      });
+
+    } catch (error) {
+        console.error('Error creating event:', error);
+    }
+};
+
+  
+
+  return (
+    <div>
+      <h2>Create Event</h2>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+
+        <TextField
+          required
+          id="outlined-required"
+          label="Title"
+          name="name"
+          value={eventData.name}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="datetime-filter"
+          label="Start Time"
+          name="startTime"
+          type="datetime-local"
+          value={eventData.startTime}
+          onChange={handleChange}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{ m: 1 }}
+        />
+        <TextField
+          required
+          id="datetime-filter"
+          label="End Time"
+          name="endTime"
+          type="datetime-local"
+          value={eventData.endTime}
+          onChange={handleChange}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{ m: 1 }}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Location"
+          name="location"
+          value={eventData.location}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Description"
+          name="description"
+          value={eventData.description}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Category"
+          name="category"
+          value={eventData.category}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="BannerImageURL"
+          name="bannerImage"
+          value={eventData.bannerImage}
+          onChange={handleChange}
+        />
+      </Box>
+      <Button variant="contained" type="submit" onClick={handleSubmit} disabled={isEventDataEmpty}>Submit</Button>
+    </div>
+  );
+};
+
+export default EventCreationForm;
